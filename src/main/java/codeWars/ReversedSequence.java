d53c3ead2ac810cbb2e96f81fe197589bf015ff9
ELİF DENK <elifdenkk@gmail.com>
2023-10-27 14:35:17 +0300

    ReversedSequence

---
+++ src/main/java/codeWars/ReversedSequence.java
@@ -0,0 +1,19 @@
+package codeWars;
+
+public class ReversedSequence {
+    public static int[] reverse(int n){
+
+        int[] array = new int[n];
+        for(int i=0; i<n;i++){
+            array[i]=n-i;
+        }
+        return array;
+    }
+
+
+}
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+ */
