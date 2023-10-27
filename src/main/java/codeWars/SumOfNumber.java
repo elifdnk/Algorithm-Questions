b3c12f6fef88c3f411c96a79076df6a85e602732
ELİF DENK <elifdenkk@gmail.com>
2023-10-27 14:02:21 +0300

    SumOfNumbers

---
+++ src/main/java/codeWars/SumOfNumber.java
@@ -0,0 +1,20 @@
+package codeWars;
+
+public class SumOfNumber {
+
+    public static int summation(int n) {
+
+        return  n*(n+1)/2;
+    }
+
+
+
+    public static int summation2(int n) {
+
+        int sum =0;
+        for (int i = 0; i <=n ; i++) {
+            sum +=i;
+        }
+        return sum;
+    }
+}
