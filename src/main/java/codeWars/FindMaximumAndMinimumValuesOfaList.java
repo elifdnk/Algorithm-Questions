522b1594432087c1b9b604542c7994b968073db3
ELİF DENK <elifdenkk@gmail.com>
2023-10-27 14:26:39 +0300

    FindMaximumAndMinimumValuesOfaList

---
+++ src/main/java/codeWars/FindMaximumAndMinimumValuesOfaList.java
@@ -0,0 +1,46 @@
+package codeWars;
+
+import java.util.Arrays;
+
+public class FindMaximumAndMinimumValuesOfaList {
+
+    public int min(int[] list) {
+        int min = list[0];
+        for(int i=1;i<list.length;i++){
+            if(list[i]<min)  min= list[i];
+        }
+        return min;
+    }
+
+    public int max(int[] list) {
+        int max = list[0];
+        for(int i=1;i<list.length;i++){
+            if(list[i]>max)  max= list[i];
+        }
+        return max;
+    }
+
+    //--------------------------------------------------------------//
+    public int min2(int[] list) {
+        return Arrays.stream(list).min().getAsInt();
+    }
+
+    public int max2(int[] list) {
+        return Arrays.stream(list).max().getAsInt();
+    }
+
+
+
+
+}
+/*
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+Notes
+You may consider that there will not be any empty arrays/vectors.
+ */
